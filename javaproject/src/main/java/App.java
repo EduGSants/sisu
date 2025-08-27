6ed88583e2d2c1de696b196301e0595f71a4962a
Edu Gabriel <edu.santos@dcomp.ufs.br>
2025-08-27 21:10:17 +0000

    Coloquei um comentário

--- javaproject/src/main/java/App.java
+++ javaproject/src/main/java/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main (String[] args) {
         List<Curso> cursos = leitura.inserirPessoas("/workspaces/sisu/javaproject/src/main/resources/dados.csv");
         for(Curso s : cursos) {
-            System.out.println(s.getNomeCurso());
+            System.out.println(s.getNomeCurso());   // Não vamos utilizar isso, é somente para teste
         }
     }
 }
