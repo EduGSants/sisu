b615f93ef062667559261c0f62bf2171041e8d9b
Edu Gabriel <edu.santos@dcomp.ufs.br>
2025-08-28 18:30:39 -0300

    Update App.java
    Mudanças realizadas apenas para verificar a funcionalidade dos métodos feitos

--- javaproject/src/main/java/App.java
+++ javaproject/src/main/java/App.java
@@ -1,12 +1,27 @@
 import service.*;
 import model.*;
+
 import java.util.List;
+import java.math.BigDecimal;
 
 public class App {
     public static void main (String[] args) {
         List<Curso> cursos = leitura.inserirPessoas("/workspaces/sisu/javaproject/src/main/resources/dados.csv");
+        // Não vamos utilizar o que vier abaixo, são apenas para testes
         for(Curso s : cursos) {
-            System.out.println("Curso: " + s.toString() + ", Campus: " + s.getCampus());   // Não vamos utilizar isso, é somente para teste
+            System.out.println(s.toString() + "," + s.getCampus());
+        }
+        List<BigDecimal> porcentagens = estatisticas.turnos(cursos);
+        for(BigDecimal s : porcentagens) {
+            System.out.println(s + "%");
+        }
+        porcentagens = estatisticas.medias(cursos);
+        for(BigDecimal media : porcentagens) {
+            System.out.println(media);
+        }
+        porcentagens = estatisticas.demandas(cursos);
+        for(BigDecimal s : porcentagens) {
+            System.out.println(s + "%");
         }
     }
 }
