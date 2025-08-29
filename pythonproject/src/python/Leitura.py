8b6e513970c09472f00ed3cb171692f542649e66
Edu Gabriel <edu.santos@dcomp.ufs.br>
2025-08-28 21:35:12 -0300

    Create Leitura.py

---
+++ pythonproject/src/python/Leitura.py
@@ -0,0 +1,28 @@
+from typing import List
+from python import Pessoa
+from python import Curso
+
+class Leitura:
+    @staticmethod
+    def addvariaveis(caminho: str) -> List[Curso]:
+        pessoas: List[Pessoa] = []
+        cursos: List[Curso] = []
+
+        try:
+            with open(caminho, "r", encoding="utf-8") as input_file:
+                next(input_file) # pular o cabeçalho
+
+                for line in input_file:
+                    parts = line.strip().split(",",5)
+                    curso = Curso(parts[1],parts[2])
+
+                    if curso not in cursos:
+                        cursos.append(curso)
+                    else:
+                        curso = cursos[cursos.index(curso)]
+                    a = Pessoa(parts[0],curso.getNomeCurso, curso.getCampus, float(parts[4]), parts[3], int(parts[5]))
+                    curso.candidatos.append(a)
+                    pessoas.append(a)
+        except Exception as e:
+            print("Erro, culpa de Migas", e)
+        return cursos
