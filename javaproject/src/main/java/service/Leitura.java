fd4defc75d8fd8f3a5a85ba2cd1c9f2768486793
PauloGuilherme-BR <pauloob.ribeiro@gmail.com>
2025-08-29 17:14:25 -0300

    mudei o Leitura pra casar com a interface

--- javaproject/src/main/java/service/Leitura.java
+++ javaproject/src/main/java/service/Leitura.java
@@ -5,27 +5,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+// -------------------------------------------------
 
-public class Leitura {
+public class leitura {
     public static List<Curso> inserirPessoas(String caminhoArquivo) {
         List<Pessoa> pessoas = new ArrayList<>();
         List<Curso> cursos = new ArrayList<>();
 
         try (BufferedReader input = new BufferedReader(new FileReader(caminhoArquivo))) {
-            // pula cabeçalho
             input.readLine();
-
             String line;
             while ((line = input.readLine()) != null) {
                 String[] parts = line.split(",", 6);
-
                 Curso curso = new Curso(parts[1], parts[2]);
-
-                // Se o curso não existe ainda, adiciona
                 if (!cursos.contains(curso)) {
                     cursos.add(curso);
                 } else {
-                    // Se já existe, pega o curso real da lista
                     curso = cursos.get(cursos.indexOf(curso));
                 }
 
@@ -38,12 +33,13 @@ public class Leitura {
                     Integer.parseInt(parts[5])
                 );
 
-                curso.candidatos.add(a);
+                curso.addCandidato(a);
                 pessoas.add(a);
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
 
         return cursos;
